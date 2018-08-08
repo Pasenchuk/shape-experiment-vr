7c71c97fac55b2d321b5ddb5050889ce1a5ff390
Victor Pasenchuk <victor.pasenchuk@gmail.com>
2018-08-08 14:52:46 +0600

    Add selection from file, rename assets, disable VR

--- Assets/RorshahController.cs
+++ Assets/RorshahController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.IO;
+using System.Collections;
 
 public class RorshahController : MonoBehaviour
 {
@@ -32,7 +33,9 @@ public class RorshahController : MonoBehaviour
 
     private int target = 10;
 
+    private int[] order;
 
+    private ArrayList log = new ArrayList();
 
     void Awake()
     {
@@ -43,8 +46,17 @@ public class RorshahController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        Debug.Log(Application.persistentDataPath + "file");
+
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        var orderStr = LoadFile("input.txt").Split(' ');
+        order = new int[orderStr.Length];
+        for (int i = 0; i < order.Length; i++)
+        {
+            order[i] = int.Parse(orderStr[i]);
+        }
+        Debug.Log(order);
         Debug.Log(System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
+
         rend1 = panel1.GetComponent<Renderer>();
         rend1.material.mainTexture = blackTexture;
 
@@ -77,40 +89,42 @@ public class RorshahController : MonoBehaviour
                 case Phase.ENTRY:
                     targetTime = Time.time + pictureCooldown;
                     currentPhase = Phase.PICTURE;
-                    rend1.material.mainTexture = textures[pos % textures.Length];
-                    rend2.material.mainTexture = textures[pos % textures.Length];
+                    rend1.material.mainTexture = textures[order[pos]];
+                    rend2.material.mainTexture = textures[order[pos]];
                     break;
                 case Phase.PICTURE:
                     targetTime = Time.time + pauseCooldown;
                     currentPhase = Phase.PAUSE;
-                    if (subPhase < subPhasesCount - 1)
-                    {
-                        rend1.material.mainTexture = blackTexture;
-                        rend2.material.mainTexture = blackTexture;
-                    }
-                    else
-                    {
-                        rend1.material.mainTexture = endTexture;
-                        rend2.material.mainTexture = endTexture;
-                    }
+
+                    rend1.material.mainTexture = blackTexture;
+                    rend2.material.mainTexture = blackTexture;
                     break;
                 case Phase.PAUSE:
                     subPhase++;
                     if (subPhase < subPhasesCount)
                     {
                         targetTime = Time.time + pictureCooldown;
-                        rend1.material.mainTexture = textures[pos % textures.Length];
-                        rend2.material.mainTexture = textures[pos % textures.Length];
+                        rend1.material.mainTexture = textures[order[pos]];
+                        rend2.material.mainTexture = textures[order[pos]];
                         currentPhase = Phase.PICTURE;
                     }
                     else
                     {
                         pos++;
-                        subPhase = 0;
-                        targetTime = Time.time + entryCooldown;
-                        rend1.material.mainTexture = blackTexture;
-                        rend2.material.mainTexture = blackTexture;
-                        currentPhase = Phase.ENTRY;
+                        if (pos < order.Length)
+                        {
+                            subPhase = 0;
+                            targetTime = Time.time + entryCooldown;
+                            rend1.material.mainTexture = blackTexture;
+                            rend2.material.mainTexture = blackTexture;
+                            currentPhase = Phase.ENTRY;
+                        }
+                        else
+                        {
+
+                            rend1.material.mainTexture = endTexture;
+                            rend2.material.mainTexture = endTexture;
+                        }
                     }
                     break;
             }
@@ -118,40 +132,48 @@ public class RorshahController : MonoBehaviour
     }
 
 
-     public void SaveFile(string name, string content)
+    public void SaveFile(string name, string[] content)
     {
         string destination = Application.persistentDataPath + "/" + name;
         FileStream file;
 
-        if (File.Exists(destination)) file = File.OpenWrite(destination);
-        else file = File.Create(destination);
-
-        //file.w
-
-        //GameData data = new GameData(currentScore, currentName, currentTimePlayed);
-        //BinaryFormatter bf = new BinaryFormatter();
-        //bf.Serialize(file, data);
-        file.Close();
-    }
-
-    public void LoadFile()
-    {
-        string destination = Application.persistentDataPath + "/save.dat";
-        FileStream file;
-
-        if (File.Exists(destination)) file = File.OpenRead(destination);
-        else
+        if (!File.Exists(destination))
         {
-            Debug.LogError("File not found");
-            return;
+            file = File.Create(destination);
+            file.Close();
         }
 
-        //string path = "Assets/Resources/test.txt";
+        StreamWriter writer = new StreamWriter(destination);
+        Debug.Log(content);
+        foreach (string s in content)
+        {
+            Debug.Log(s);
+            writer.WriteLine();
+        }
+        writer.Flush();
+        writer.Close();
+    }
 
-        ////Read the text from directly from the test.txt file
-        //StreamReader reader = new StreamReader(path);
-        //Debug.Log(reader.ReadToEnd());
-        //reader.Close();
+    public string LoadFile(string name)
+    {
+        string destination = Application.persistentDataPath + "/" + name;
+
+        Debug.Log(destination);
+
+        if (File.Exists(destination))
+        {
+            StreamReader reader = new StreamReader(destination);
+            string result = reader.ReadToEnd();
+            reader.Close();
+            return result;
+        }
+        else
+        {
+            Debug.Log("File not found");
+            string defaultRes = "1 5 13 10";
+            SaveFile(name, new string[1] { defaultRes });
+            return defaultRes;
+        }
     }
 
     enum Phase
